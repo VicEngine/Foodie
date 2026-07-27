97298b43210aa50130e58272c5e2d2d41d5c4137
Victor Abass <owooluwaabass3@gmail.com>
2026-07-27 19:19:05 -0400

    landing

--- App.js
+++ App.js
@@ -1,6 +1,27 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { useEffect } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import MainNavigator from './src/navigator/MainNavigator';
+
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    OpenSans: require('./src/assets/fonts/OpenSans-VariableFont_wdth,wght.ttf'),
+    'OpenSans-Italic': require('./src/assets/fonts/OpenSans-Italic-VariableFont_wdth,wght.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <MainNavigator />
--- src/Screens/Landingpage.jsx
+++ src/Screens/Landingpage.jsx
@@ -1,10 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native'
 import React from 'react'
 
 const Landingpage = () => {
   return (
     <View style={styles.container}>
-      <Text>Landingpage</Text>
+      <ImageBackground>
+        <View style={styles.step1}>
+          <Image source={require('../assets/images/Ellipse3.png')} />
+        </View>
+        <View style={styles.step2}>
+          <Image source={require('../assets/images/Ellipse2.png')} />
+        </View>
+        <View style={styles.step3}>
+          <Image source={require('../assets/images/Ellipse1.png')} />
+        </View>
+      </ImageBackground>
+      <View style={styles.step4}>
+        <Image source={require('../assets/images/bbg1.png')} />
+      </View>
+      <View style={styles.step5}>
+        <Text style={styles.title}>
+          Your Food, Lightning Fast
+        </Text>
+        <Image source={require('../assets/images/bbg2.png')}/>
+      </View>
     </View>
   )
 }
@@ -12,8 +31,41 @@ const Landingpage = () => {
 export default Landingpage
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'magenta'
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF'
+  },
+  step1: {
+    position: 'relative',
+    right: 8
+  },
+  step2: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: -5,
+  },
+  step3: {
+    position: 'absolute',
+    top: 14,
+    bottom: 0,
+    left: -5,
+  },
+  step4: {
+    transform: [{
+      translateY: 30,
+    }],
+    alignItems: 'center'
+  },
+  step5:{
+    marginTop:50,
+    flexDirection:'row',
+    alignItems:'center'
+  },
+  title: {
+    fontFamily: 'OpenSans',
+    fontSize: 24,
+    fontWeight: '600',
+    color: '#1F2937',
+  },
 })
