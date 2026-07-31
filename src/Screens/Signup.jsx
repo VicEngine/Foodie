6e5180ad6a8d8ecfd147cba171e059b77bd4bf0a
Victor Abass <owooluwaabass3@gmail.com>
2026-07-31 08:40:18 -0400

    doneLogin

--- src/Screens/Signup.jsx
+++ src/Screens/Signup.jsx
@@ -1,49 +1,98 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput, ScrollView } from 'react-native'
 import React from 'react'
 import Bgg from '../components/Bgg'
 
 const Signup = () => {
+
     return (
         <View style={styles.container}>
-            <Bgg />
-            <Image source={require('../assets/images/bbg3.png')} style={styles.step1} />
-            <TouchableOpacity style={styles.step2}>
-                <View style={styles.step2Sub}>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Bgg />
+                <Image source={require('../assets/images/bbg3.png')} style={styles.step1} />
+                <TouchableOpacity style={styles.step2}>
+                    <View style={styles.step2Sub}>
+                    </View>
+                    <View style={styles.step2Sub1} />
+                    <Text style={styles.title}>whats your address?</Text>
+                </TouchableOpacity>
+                <View style={styles.step3Con}>
+                    <View style={styles.step3}>
+                        <Image source={require('../assets/icons/Star1.png')} />
+                    </View>
+                    <Text style={styles.title2}>
+                        Use current location
+                    </Text>
                 </View>
-                <View style={styles.step2Sub1} />
-                <Text style={styles.title}>whats your address?</Text>
-            </TouchableOpacity>
-            <View style={styles.step3Con}>
-                <View style={styles.step3}>
-                    <Image source={require('../assets/icons/Star1.png')} />
-                </View>
-                <Text style={styles.title2}>
-                    Use current location
-                </Text>
-            </View>
-            <View style={styles.step4}>
-                <View style={styles.step5Con}>
-                    <View style={styles.step5}>
-                        <Text style={styles.title3}>
-                            Welcome
-                        </Text>
+                <View style={styles.step4}>
+                    <View style={styles.step5Con}>
+                        <View style={styles.step5}>
+                            <Text style={styles.title3}>
+                                Welcome
+                            </Text>
 
-                        <View style={styles.step6Con}>
-                            <Text>
-                                Account Already
-                            </Text>
-                            <View style={styles.step6Line} />
-                            <Text>
-                                Login
-                            </Text>
+                            <View style={styles.step6Con}>
+                                <Text>
+                                    Account Already
+                                </Text>
+                                <View style={styles.step6Line} />
+                                <Text>
+                                    Login
+                                </Text>
+                            </View>
                         </View>
+
+                        <View style={styles.step7Con}>
+                            <View style={styles.step7}>
+
+                                <Image source={require('../assets/icons/nig.png')} />
+                                <Text>
+                                    +234
+                                </Text>
+                                <Image source={require('../assets/icons/downArrow.png')} />
+                            </View>
+                            <View style={styles.step7Hold}>
+                                <TextInput
+                                    placeholder='Phone Number'
+                                    placeholderTextColor={'#000000'}
+                                    style={styles.placeHold}
+                                />
+                            </View>
+                        </View>
+
+                        <TouchableOpacity activeOpacity={0.6} style={styles.step8}>
+                            <Text style={styles.title4}>
+                                Continue
+                            </Text>
+                        </TouchableOpacity>
+
+                        <View style={styles.Conline}>
+                            <View style={styles.line} />
+                            <Text style={styles.linetitle}>or with</Text>
+                            <View style={styles.line} />
+                        </View>
+
+                        <TouchableOpacity activeOpacity={0.6} style={styles.step9}>
+                            <Image source={require('../assets/icons/google.png')} style={styles.googleIcon} />
+                            <Text style={styles.title5}>
+                                Google
+                            </Text>
+                        </TouchableOpacity>
+
                     </View>
-
-                    <View style={styles.step7Con}>
-
+                    <View style={styles.step10}>
+                        <TouchableOpacity style={styles.substep10} >
+                            <Image source={require('../assets/icons/leftArrow.png')} />
+                            <Text style={styles.back}>Back</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.title6}>Other methods</Text>
+                    </View>
+                    <View style={styles.step11}>
+                        <Text style={styles.title7}>
+                            By creating account, you automatically accept our {'\n'}Terms of service,privacy Polocy Cookies Policy
+                        </Text>
                     </View>
                 </View>
-            </View>
+            </ScrollView>
         </View>
     )
 }
@@ -119,7 +168,11 @@ const styles = StyleSheet.create({
         borderTopRightRadius: 40,
         borderTopLeftRadius: 40,
         marginTop: 20,
-        borderWidth: 2,
+        // borderRightWidth: 2,
+        // borderLeftWidth:2,
+        borderStartWidth: 1,
+        borderEndWidth: 1,
+        borderTopWidth: 1,
         borderColor: '#CAC5C5'
     },
     step5Con: {
@@ -128,7 +181,7 @@ const styles = StyleSheet.create({
     step5: {
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent:"space-between"
+        justifyContent: "space-between"
     },
     title3: {
         fontSize: 25,
@@ -145,15 +198,140 @@ const styles = StyleSheet.create({
         borderWidth: 0.5,
         borderColor: '#000000'
     },
-    step7Con:{
-        width:'100%',
-        height:60,
-        backgroundColor:'#FFFAFA',
-        elevation:3,
-        shadowOpacity:0.3,
-        marginTop:30,
-        borderRadius:20,
-        alignSelf:"center",
-        shadowOffset:0.5,
+    step7Con: {
+        width: '100%',
+        height: 60,
+        backgroundColor: '#FFFAFA',
+        elevation: 3,
+        shadowOpacity: 0.3,
+        marginTop: 30,
+        borderRadius: 20,
+        alignSelf: "center",
+        shadowOffset: 0.5,
+        gap: 50,
+        paddingHorizontal: 20,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    step7: {
+        flexDirection: "row",
+        alignItems: 'center',
+        gap: 5
+    },
+    step7Hold: {
+        flex: 1,
+    },
+    placeHold: {
+        fontSize: 15,
+        fontWeight: 'medium',
+        fontFamily: 'Inter'
+    },
+    step8: {
+        width: '100%',
+        height: 60,
+        backgroundColor: "#B72245",
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 20,
+        marginTop: 30,
+        shadowColor: '#3a3939',
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 1,
+        elevation: 6,
+
+    },
+    title4: {
+        fontSize: 15,
+        fontFamily: 'Inter',
+        fontWeight: 'bold',
+        color: '#FFFDFD',
+    },
+    Conline: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginVertical: 30
+    },
+    line: {
+        backgroundColor: '#B8B2B2',
+        width: '50%',
+        height: 1,
+        marginHorizontal: 10,
+    },
+    linetitle: {
+        fontWeight: 'light',
+        fontSize: 15,
+        fontFamily: 'Inter',
+        color: '#000000'
+    },
+
+    step9: {
+        width: '100%',
+        height: 60,
+        backgroundColor: '#FFFDFD',
+        borderRadius: 20,
+        marginTop: 30,
+        shadowColor: '#B8B2B2',
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 1,
+        elevation: 6,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+    },
+    googleIcon: {
+        marginRight: 10,
+        position: 'absolute',
+        left: 20,
+    },
+    title5: {
+        fontSize: 15,
+        fontFamily: 'Inter',
+        fontWeight: 'bold',
+        color: '#000000',
+    },
+    step10: {
+        flexDirection: "row",
+        alignItems: 'center',
+        justifyContent: "center",
+        marginTop: 20
+    },
+    substep10: {
+        position: 'absolute',
+        left: 0,
+        alignItems: 'center',
+    },
+    back: {
+        fontSize: 12,
+        fontWeight: 'medium',
+        fontFamily: 'Inter',
+        color: "#040404"
+    },
+    title6: {
+        fontSize: 13,
+        fontFamily: 'Inter',
+        fontWeight: 'bold',
+        color: '#B72245'
+    },
+    title7: {
+        textAlign: 'center',
+        fontSize: 12,
+        fontFamily: 'Inter',
+        fontWeight: 'semibold',
+        color: '#000000',
+        marginTop: 30
+    },
+    step11:{
+        flex:1,
+        justifyContent:'flex-end',
     },
 })
